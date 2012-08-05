f0d6e788b25aa28fcec1b3a471381a8ffc80bc1d
Steven Karas <steven.karas@trusteer.com>
2012-08-05 13:44:38 +0300

    added trollop adapter for option processing

---
+++ lib/thunder/options/trollop.rb
@@ -0,0 +1,24 @@
+require 'trollop'
+
+module Thunder
+  class TrollopAdapter
+    # @see Thunder#process_options
+    def self.process_options(args, command_spec)
+      return nil unless command_spec[:options]
+      Trollop.options do
+        command_spec[:options].each do |name, option_spec|
+          opt_options = {}
+          description = option_spec[:desc] || ""
+          type = option_spec[:type]
+          type = :flag if type == Thunder::Boolean
+          opt_options[:type] = type
+          default_value = option_spec[:default]
+          opt_options[:default] = default_value if default_value
+          opt_options[:short] = "-" + option_spec[:short]
+
+          opt name, description, opt_options
+        end
+      end
+    end
+  end
+end
